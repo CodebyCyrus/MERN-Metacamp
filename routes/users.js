708f2fe6726a85f3d98de48b78c22d32c945f0cf
PersianDavinci <persiandavinci@gmail.com>
2023-09-23 21:34:46 +0330

    ready to deploying

---
+++ routes/users.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const router = express.Router();
+const passport = require("passport");
+const catchAsync = require("../utils/catchAsync");
+const User = require("../models/user");
+const users = require("../controllers/users");
+
+router
+  .route("/register")
+
+  .get(users.renderRegister)
+
+  .post(catchAsync(users.register));
+
+router
+  .route("/login")
+  .get(users.renderLogin)
+  .post(
+    passport.authenticate("local", {
+      failureFlash: true,
+      failureRedirect: "/login",
+    }),
+    users.login
+  );
+
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+module.exports = router;
